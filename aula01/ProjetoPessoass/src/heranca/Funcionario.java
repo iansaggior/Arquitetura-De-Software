05c5e0ea79fc9e046595d67244a168290585f698
ian.saggior <ian.saggior.pereira@gmail.com>
2023-08-17 20:41:01 -0300

    aula01_parte01

---
+++ aula01/ProjetoPessoass/src/heranca/Funcionario.java
@@ -0,0 +1,11 @@
+package heranca;
+public class Funcionario extends PessoaFisica{
+    private int matricula;
+
+    public int getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }    
+}
---
+++ aula01/ProjetoPessoass/src/heranca/PessoaFisica.java
@@ -0,0 +1,12 @@
+package heranca;
+public class PessoaFisica extends Pessoa{
+    private long cpf;
+
+    public long getCpf() {
+        return cpf;
+    }
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+    
+}
