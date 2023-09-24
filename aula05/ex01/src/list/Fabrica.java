bffb962d1d6c89861bab7ac0922881d7a4818644
iansaggior <ian.saggior.pereira@gmail.com>
2023-09-23 22:08:19 -0300

    aula05_ex01

--- aula05/ex01/src/list/Fabrica.java
+++ aula05/ex01/src/list/Fabrica.java
@@ -3,12 +3,18 @@ package list;
 import java.util.*;
 
 public class Fabrica {
-    public static List metodoFabrica(Tipo nome) {
-        if (nome.equals(Tipo.ARRAYLIST)) {
+    public static List metodoFabrica(Tipo nome) 
+    {
+        if (nome.equals(Tipo.ARRAYLIST)) 
+        {
             return new ArrayList<>();
-        } else if (nome.equals(Tipo.LINKEDLIST)) {
+        } 
+        else if (nome.equals(Tipo.LINKEDLIST)) 
+        {
             return new LinkedList<>();
-        } else if (nome.equals(Tipo.VECTOR)) {
+        } 
+        else if (nome.equals(Tipo.VECTOR)) 
+        {
             return new Vector<>();
         }
         return null;
--- aula05/ex01/src/main/Main.java
+++ aula05/ex01/src/main/Main.java
@@ -16,17 +16,27 @@ public class Main {
         list01.add("item03AL");
         list01.add("item04AL");
 
-        list02.add("item01AL");
-        list02.add("item02AL");
-        list02.add("item03AL");
-        list02.add("item04AL");
+        list02.add("item01Lkl");
+        list02.add("item02lkl");
+        list02.add("item03Lkl");
+        list02.add("item04Lkl");
 
-        list03.add("item01AL");
-        list03.add("item02AL");
-        list03.add("item03AL");
-        list03.add("item04AL");
+        list03.add("item01Vct");
+        list03.add("item02Vct");
+        list03.add("item03Vct");
+        list03.add("item04Vct");
 
         for (Object item : list01) {
+            System.out.println(item);
+        }
+
+        System.out.println();
+        for (Object item : list02) {
+            System.out.println(item);
+        }
+        
+        System.out.println();
+        for (Object item : list03) {
             System.out.println(item);
         }
 
