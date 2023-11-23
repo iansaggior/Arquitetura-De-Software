ee860b94550a5d25e6916a5724e9b09f43f90616
iansaggior <ian.saggior.pereira@gmail.com>
2023-11-22 23:06:47 -0300

    docState

--- plataformaTarefas/src/main/java/cliente/GerenciadorTarefas.java
+++ plataformaTarefas/src/main/java/cliente/GerenciadorTarefas.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package cliente;
 
 /**
@@ -12,8 +11,13 @@ public class GerenciadorTarefas {
 
     public static void main(String[] args) {
         Tarefa tarefa = new Tarefa("Fazer elicitação e requisitos");
+        // simulando ações que vão seguir uma regra que acordo com o caminho dado, sendo elas avançar para a  
+        // ação requisitada ou dizer se já está na ação desejada/ não é possivel "voltar" para essa ação
         tarefa.requisitarAtrasada();
+        tarefa.requisitarConcluida();
+        tarefa.requisitarConcluida();
         tarefa.requisitarPendente();
         tarefa.requisitarConcluida();
+        tarefa.requisitarAtrasada();
     }
 }
--- plataformaTarefas/src/main/java/state/Concluida.java
+++ plataformaTarefas/src/main/java/state/Concluida.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package state;
-
 import cliente.*;
 
 /**
